75970e3d19ef4d67ccddde89a7407afbae8ecdf3
Jason Zinn <jason@zinnfinity.com>
2020-03-06 10:01:19 -0600

    Added membership page template

---
+++ template-apply.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template Name: Membership
+ * 
+ * The template for displaying the membership page
+ *
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package zinnfinity
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'apply' );
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
