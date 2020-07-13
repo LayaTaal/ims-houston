91fc13afe8208718fad5ce2da431f260296ad9a6
Jason Zinn <jason@zinnfinity.com>
2020-07-12 23:05:40 -0500

    updated homepage

--- front-page.php
+++ front-page.php
@@ -45,7 +45,11 @@ $events = tribe_get_events( [
 		<main id="main" class="site-main">
       <div class="frontpage-events">
         <h2>Upcoming Events</h2>
-        <?php get_template_part( 'template-parts/event', 'list' ); ?>
+        <p>Due to the imact of COVID-19, IMS Houston has had to cancel our originally planned programs. 
+        Please check back soon for our revised calendar which will feature digital performances by 
+        professional musicians from India and the U.S. as well as unique programs to highlight Houston's own artists.
+        </p> 
+        <?php //get_template_part( 'template-parts/event', 'list' ); ?>
       </div>
     </main><!-- #main -->
     <?php get_sidebar() ?>
